e8eb029378541e469d11e99f1924f4e0e02fefab
Matheus Sottelli <matheus.sales@sottelli.com>
2021-06-02 21:27:35 -0300

    User

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function getAll(){
+        return User::with('students')->get();
+    }
+
+    public function get($id){
+        return User::with('students')->find($id);
+    }
+
+    public function create(Request $request) {
+        $user = User::create($request->all());
+
+        return response()->json($user, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+
+        return response()->json($user, 200);
+    }
+
+    public function delete(Request $request, $id) {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +22,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::fallback(function(){
     return response()->json(['message' => 'Page Not Found'], 404);
 });
+
 Route::get('/students', [StudentController::class, 'getAll']);
 Route::get('/students/{id}', [StudentController::class, 'get']);
 Route::post('/students', [StudentController::class, 'create']);
 Route::put('/students/{id}', [StudentController::class, 'update']);
 Route::delete('/students/{id}', [StudentController::class, 'delete']);
+
+Route::get('/users', [UserController::class, 'getAll']);
+Route::get('/users/{id}', [UserController::class, 'get']);
+Route::post('/users', [UserController::class, 'create']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'delete']);
